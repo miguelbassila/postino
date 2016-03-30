0d2d8f016f5449271aa2f6287c16bd24103d464c
Miguel Bassila <miguelbassila@me.com>
2016-03-29 22:39:06 -0300

    Add project initial files

---
+++ Postino/Postino.h
@@ -0,0 +1,19 @@
+//
+//  Postino.h
+//  Postino
+//
+//  Created by Miguel Bassila on 3/29/16.
+//  Copyright © 2016 Miguel Bassila. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Postino.
+FOUNDATION_EXPORT double PostinoVersionNumber;
+
+//! Project version string for Postino.
+FOUNDATION_EXPORT const unsigned char PostinoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Postino/PublicHeader.h>
+
+
